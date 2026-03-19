717ac701ad0999f435f2d8c19babb516df529805
Jocelyn12344444 <joselynmartinezurcino@gmail.com>
2026-03-19 16:05:09 -0700

    Arreglo

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import post, category, comment
+from .models import Post, category, comment
 
 
-admin.site.register(post)
+admin.site.register(Post)
 admin.site.register(category)
 admin.site.register(comment)
